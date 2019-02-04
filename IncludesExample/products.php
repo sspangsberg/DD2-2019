3e7ee9932dc77d78e45bb9d26b73f7d407bea3c3
Søren Spangsberg Jørgensen <spangsberg@Srens-MacBook-Pro.local>
2019-02-04 09:51:47 +0100

    Added Includes examle with vanilla JS / jQuery / PHP samples

---
+++ IncludesExample/products.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("header.html"); ?> 
+</head>
+<body>
+    
+    <?php include("navigation.html"); ?>
+
+
+
+      <br><br>
+      <h1>Products</h1>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    
+   
+    
+</body>
+</html>
